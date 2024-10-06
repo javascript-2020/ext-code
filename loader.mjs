e04fdc4f5a4d25004c6d022559e91d243d8e8397
matt <javascript.12.03.2024@gmail.com>
2024-10-07 00:21:28 +0100

    Update loader.mjs

--- loader.mjs
+++ loader.mjs
@@ -1,5 +1,7 @@
 /*
-import ext from 'https://raw.githubusercontent.com/javascript-2020/ext-code/main/loader.mjs
+
+import ext from 'https://ext-code-y2xn13bvx5py.runkit.sh/';
+
 */
 
 var ext    =  await fetch('https://raw.githubusercontent.com/javascript-2020/ext-code/main/loader.js').then(res=>res.text().then(eval));
